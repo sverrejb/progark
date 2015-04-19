cf7646580f15b5c00382aeb51f740ced114a9bfe
lindkjop <pialindkjolen@gmail.com>
2015-04-20 01:40:30 +0200

    created Message class

---
+++ MafiaGame/app/src/main/java/progark/mafia/mafiagame/models/Message.java
@@ -0,0 +1,29 @@
+package progark.mafia.mafiagame.models;
+
+import progark.mafia.mafiagame.utils.Event;
+
+/**
+ * Created by pialindkjolen on 20.04.15.
+ */
+public class Message extends Event {
+
+    @Override
+    public void receiveEvent() {
+        
+    }
+
+    @Override
+    public void sendEvent() {
+
+    }
+
+    @Override
+    public void serialize() {
+
+    }
+
+    @Override
+    public void deserialize() {
+
+    }
+}
