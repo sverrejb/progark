09a073639eafe2a30701aacd0945a5f1944c2742
DyHex <maggmadon@gmail.com>
2015-04-21 22:29:57 +0200

    redone the voting system

--- MafiaGame/app/src/main/java/progark/mafia/mafiagame/controller/VotingSystem.java
+++ MafiaGame/app/src/main/java/progark/mafia/mafiagame/controller/VotingSystem.java
@@ -1,87 +1,91 @@
 package progark.mafia.mafiagame.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-import progark.mafia.mafiagame.controller.GameLogic;
+import progark.mafia.mafiagame.connection.Event;
 import progark.mafia.mafiagame.models.Phases.AbstractPhase;
 import progark.mafia.mafiagame.models.Player;
-import progark.mafia.mafiagame.models.Vote;
 
 /**
  * Created by Magnus on 20.04.2015.
  */
 public class VotingSystem{
 
-    ArrayList<Player> playersAlive;
-    ArrayList<AbstractPhase> phases;
-    AbstractPhase phase;
+    ArrayList<Player> playersAlive;         // All players that are still alive
+    AbstractPhase phase;                    // The phase the game is in
     GameLogic gl;
 
-    ArrayList<Player> voters;
-    ArrayList<Vote> votes;
-    int[] voteCount;
+    HashMap<String, Player> whoCanVote;
 
-    public VotingSystem(GameLogic gl, ArrayList playersInGame, ArrayList phases, AbstractPhase phase){
-        this.gl = gl;
+    HashMap<String, Integer> votes;         // vote count
+    HashMap<String, String> whatWasVoted;   // Who voted on who
+
+    String target;
+
+    public VotingSystem(GameLogic gameLogic, ArrayList playersInGame, AbstractPhase phase){
         this.playersAlive = playersInGame;
-        this.phases = phases;
         this.phase = phase;
+        gl = gameLogic;
 
-        voters = new ArrayList<>();
-        votes = new ArrayList<>();
-        voteCount = new int[this.playersAlive.size()];
+        whoCanVote = new HashMap<>();
+        votes = new HashMap<>();
+        whatWasVoted = new HashMap<>();
+
+        sortVotersOnCurrentPhase();
+        initiateVoting();
+
+        target = "";
     }
 
     // List of players in, list of eligible players out
-    private void sortVotersOnCurrentPhase(ArrayList<Player> playersAlive){
+    private void sortVotersOnCurrentPhase(){
 
         for (int i = 0; i < playersAlive.size(); i++){
-            if(phase.getParticipatingRoles().contains(playersAlive.get(i).getRole()))
-                voters.add(playersAlive.get(i));
+            if(phase.getParticipatingRoles().contains(playersAlive.get(i).getRole())) {
+                whoCanVote.put(playersAlive.get(i).getId(), playersAlive.get(i));
+            }
+            votes.put(playersAlive.get(i).getId(), 0); // adds all player IDs to the hashmap
         }
 
     }
 
     private void countVotes(){
 
-        int majority = 0;
-        int majoritiesIndex = 0;
+        int highest = -1;
+        String h = "";
 
-        if(votes.size()>1)
-            gl.voteComplete(votes.get(0).getTarget(), votes.get(0).getPerformer());
-
-        else{
-            for(int i = 0; i < votes.size(); i++){
-                voteCount[playersAlive.indexOf(votes.get(i).getTarget())] += 1;
+        for(String k : votes.keySet()){
+            if(votes.get(k)> highest) {
+                highest = votes.get(k);
+                h = k;
             }
-
-            for(int i = 0; i < voteCount.length; i++){
-                if(voteCount[i] > majority){
-                    majority = voteCount[i];
-                    majoritiesIndex = i;
-                }
-                else if(voteCount[i] == majority)
-                    System.out.print("Error: Vote Draw! Perform a revote.");
-            }
-
-            gl.voteComplete(playersAlive.get(majoritiesIndex), null);
 
         }
 
+        gl.voteComplete(PlayerObject, null);    // needs to send the player that got the majority of the votes
+                                                // null if more than one player voted, if only one player voted "doctor" he will be in the array..
     }
 
-    // push voting screen on all devices.
+    // sends a message to all clients with a list with the IDs to all who can vote
     private void initiateVoting(){
 
     }
 
     // wait on votes and initiate count when all votes have been received or time runs out.
-    private void receiveVote(Vote vote){
+    private void receiveVote(Event vote){
 
-        if(voters.contains(vote.getPerformer())){
-            votes.add(vote);
-            voters.remove(vote.getPerformer());
+        // fieldOne: the target of the vote
+        // [] fieldTwo: index 0 = the vote performer
+
+        if(votes.containsKey(vote.fieldOne) && whoCanVote.size() > 0){
+            votes.put(vote.fieldOne, votes.get(vote.fieldOne + 1));
+            whoCanVote.remove(vote.fieldTwo[0]);
+            if(whoCanVote.size() > 0)
+                countVotes();
         }
+        else
+            countVotes();
 
     }
 
