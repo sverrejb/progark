9d95ae0ebf9cbfaa214c2c968dc6fc702faa8948
Sverre Johann Bjørke <sverrejb@gmail.com>
2015-04-22 16:08:15 +0200

    fixed derped styles.xml

--- MafiaGame/app/src/main/java/progark/mafia/mafiagame/GameOverFragment.java
+++ MafiaGame/app/src/main/java/progark/mafia/mafiagame/GameOverFragment.java
@@ -34,14 +34,6 @@ public class GameOverFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
     private ClientController clientController;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GameOverFragment.
-     */
     public static GameOverFragment newInstance(String winning) {
         GameOverFragment fragment = new GameOverFragment();
         Bundle args = new Bundle();
